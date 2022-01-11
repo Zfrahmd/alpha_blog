8a6c03f5cc3d653546626561b20732f2e2b9c85d
Zafir <zfr.ahmd5661@gmail.com>
2022-01-11 15:23:18 +0500

    CRUD functions with views added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,51 @@
 class ArticlesController < ApplicationController
+
+    # show article according to their id
     def show
-        # @ is used to make article an instance of Article
-        # params[:id] are the hash parameters where id is the actual 
-        # parameter to be evaluated
-        @article = Article.find(params[:id])
+      # @ is used to make article an instance variable of Table
+      # params[:id] are the hash parameters where id is the actual 
+      # parameter to be evaluated
+      @article = Article.find(params[:id])
     end
 
+    # get all the articles
     def index
-        @articles = Article.all
+      @articles = Article.all
+    end
+
+    # display new form page for creating article
+    def new
+      # create instance of article
+      @article = Article.new
+    end
+
+    def edit
+      # find article instance to be edited and display edit form page
+      @article = Article.find(params[:id])
+    end
+
+    def create
+      # create new article with required fields
+      @article = Article.new(params.require(:article).permit(:title, :description))
+      # save artical in db
+      if @article.save
+        # rails helper - flash messages
+        flash[:notice] = "Article was Saved Successfully"
+        # on saving redirect to the page of article created
+        #redirect_to article_path(@article)
+        redirect_to @article
+      else
+        render 'new'
+      end
+    end
+
+    def update
+      @article = Article.find(params[:id])
+      if @article.update(params.require(:article).permit(:title, :description))
+        flash[:notice] = "Artical was updated successfully"
+        redirect_to @article
+      else
+        render 'edit'
+      end
     end
 end
