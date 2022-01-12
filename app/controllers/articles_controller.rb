15d8dc3223ef4c100a4bcd06a8da7d19678d26a8
Zafir <zfr.ahmd5661@gmail.com>
2022-01-12 14:29:48 +0500

    Make app production ready

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,51 +1,76 @@
 class ArticlesController < ApplicationController
 
-    # show article according to their id
-    def show
-      # @ is used to make article an instance variable of Table
-      # params[:id] are the hash parameters where id is the actual 
-      # parameter to be evaluated
-      @article = Article.find(params[:id])
-    end
+  before_action :find_article, only: [:show, :edit, :update, :destroy]
 
-    # get all the articles
-    def index
-      @articles = Article.all
-    end
+  # show article according to their id
+  def show
+    # before_action helper will run find_artcile
+    # function here
+  end
 
-    # display new form page for creating article
-    def new
-      # create instance of article
-      @article = Article.new
-    end
+  # get all the articles
+  def index
+    @articles = Article.all
+  end
 
-    def edit
-      # find article instance to be edited and display edit form page
-      @article = Article.find(params[:id])
-    end
+  # display new form page for creating article
+  def new
+    # create instance of article
+    @article = Article.new
+  end
 
-    def create
-      # create new article with required fields
-      @article = Article.new(params.require(:article).permit(:title, :description))
-      # save artical in db
-      if @article.save
-        # rails helper - flash messages
-        flash[:notice] = "Article was Saved Successfully"
-        # on saving redirect to the page of article created
-        #redirect_to article_path(@article)
-        redirect_to @article
-      else
-        render 'new'
-      end
-    end
+  # find article instance to be edited and display edit form page
+  def edit
+    # before_action helper will run find_artcile
+    # function here
+  end
 
-    def update
-      @article = Article.find(params[:id])
-      if @article.update(params.require(:article).permit(:title, :description))
-        flash[:notice] = "Artical was updated successfully"
-        redirect_to @article
-      else
-        render 'edit'
-      end
+  def create
+    # create new article with required fields
+    @article = Article.new(article_params)
+    # save artical in db
+    if @article.save
+      # rails helper - flash messages
+      flash[:notice] = "Article was Saved Successfully"
+      # on saving redirect to the page of article created
+      #redirect_to article_path(@article)
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
     end
+  end
+
+  def update
+    # before_action helper will run find_artcile
+    # function here
+    if @article.update(article_params)
+      flash[:notice] = "Artical was updated successfully"
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    # before_action helper will run find_artcile
+    # function here
+    @article.destroy
+    params[:id] = nil
+    flash[:danger] = "Article was successfully deleted"
+    redirect_to articles_path
+  end
+  
+  # set private function
+  private
+  def find_article
+    # @ is used to make article an instance variable of Table
+    # params[:id] are the hash parameters where id is the actual 
+    # parameter to be evaluated
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
   root 'pages#home'
-  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
+
+  # use only selective routes from resource article
+  #resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
+  
+  # use all routes from articles except destroy
+  resources :articles, except: [:destroy]
+  
+  # create custom routes for destroy
+  # name_route 'route_url' => controller#function, as: defining_path_text
+  delete 'articles/:id/delete' => 'articles#destroy', as: 'articles_delete'
+  get '/articles/:id/delete' => 'articles#destroy'
+  
+
 end
